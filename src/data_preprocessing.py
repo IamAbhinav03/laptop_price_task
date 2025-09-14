0e072757d4410d6331a2ab04d3778ec01afaa797
Sarthak Rathi <rathisarthak88@gmail.com>
2025-09-15 03:41:57 +0530

    feat: cpu feature extraction function

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -39,3 +39,23 @@ class Preprocessor:
         # Calculate Pixels Per Inch (PPI)
         df['ppi'] = (((df['x_res']**2) + (df['y_res']**2))**0.5 / df['inches']).astype(float)
         return df
+    
+    def _process_cpu_features(self, df):
+        """Extracts brand and tier from the 'cpu' column."""
+        df['cpu_brand'] = df['cpu'].apply(lambda x: x.split()[0])
+        
+        def extract_cpu_tier(cpu_str):
+            if "Intel Core i7" in cpu_str or "Intel Core i9" in cpu_str:
+                return "Intel Core i7/i9"
+            elif "Intel Core i5" in cpu_str:
+                return "Intel Core i5"
+            elif "Intel Core i3" in cpu_str:
+                return "Intel Core i3"
+            elif "AMD Ryzen" in cpu_str:
+                # Captures Ryzen 3, 5, 7, 9
+                return "AMD Ryzen"
+            else:
+                return "Other" # Catches Celeron, Pentium, AMD A-series etc.
+        
+        df['cpu_tier'] = df['cpu'].apply(extract_cpu_tier)
+        return df
